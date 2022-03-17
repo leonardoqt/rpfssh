f22e715410e3e426bfccad872fe3c50549b6d0f3
Tian Qiu <tianq@sas.upenn.edu>
2022-03-17 14:57:07 -0400

    print out surface

--- src/main.cpp
+++ src/main.cpp
@@ -74,13 +74,15 @@ int main()
 	double t_eion;
 	vec t_eele,tmpv1,tmpv2;
 	mat tmpm1;
+	cout.precision(10);
+	cout.setf(ios::fixed);
 	for(auto m1:t_xx)
 	{
 		t_x(0) = m1;
 		HH.ionic(t_x,t_eion,tmpv1);
 		HH.E_Hf_pd(t_x,t_p,t_eele,tmpm1,tmpv1,tmpv2);
-		cout<<m1;
-		(t_eele+t_eion).t().print();
+		cout<<m1<<' ';
+		(t_eele+t_eion).t().raw_print();
 	}
 	//-----------
 	for (int iv = 0; iv<nek; iv++)
